22f9114afb4e33648ca4685a94ecf22145e134e9
Robert Bledsaw III <rbledsaw3@outlook.com>
2025-01-01 19:57:00 -0500

    made port number const

--- main.go
+++ main.go
@@ -6,13 +6,16 @@ import (
 )
 
 func main () {
+    const port = "8080"
+
     mux := http.NewServeMux()
+    
     srv := &http.Server {
-        Addr: ":8080",
+        Addr: ":" + port,
         Handler: mux,
     }
 
-    log.Println("Starting server on :8080...")
+    log.Printf("Starting server on %s\n", port)
     log.Fatal(srv.ListenAndServe())
 
 }
