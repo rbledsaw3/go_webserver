e7836130325cc3a1a01dc05a67a13b6e44ab09f2
Robert Bledsaw III <rbledsaw3@outlook.com>
2025-01-04 13:30:45 -0500

    feat: add readiness endpoint and update file server path
    - Implemented /healthz endpoint to return 200 OK with appropriate headers and body.
    - Updated file server to handle requests at /app/ and stripped prefix using StripPrefix.

--- main.go
+++ main.go
@@ -11,6 +11,12 @@ func main () {
 
     mux := http.NewServeMux()
     mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+    mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+    mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+        w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+        w.WriteHeader(http.StatusOK)
+        w.Write([]byte("OK"))
+    })
     
     srv := &http.Server {
         Addr: ":" + port,
