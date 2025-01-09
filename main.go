66db82a390f1830d90f71715003c1a082830f0b7
Robert Bledsaw III <rbledsaw3@outlook.com>
2025-01-08 19:06:39 -0500

    feat: add POST /api/validate_chirp endpoint
    - Add JSON request handling for chirp validation
    - Implement 140 character length check
    - Return appropriate error responses

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ type apiConfig struct {
     fileserverHits atomic.Int32
 }
 
-func main () {
+func main() {
     const filepathRoot = "."
     const port = "8080"
 
@@ -20,37 +20,19 @@ func main () {
 
     mux := http.NewServeMux()
     fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
-    rootHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        if r.URL.Path == "/" {
-            http.Redirect(w, r, "/app/", http.StatusFound)
-            return
-        }
-        http.NotFound(w, r)
-    })
+    mux.Handle("/app/", fsHandler)
 
-    mux.HandleFunc("GET /", rootHandler)
-    mux.Handle("GET /app/", fsHandler) 
     mux.HandleFunc("GET /api/healthz", handlerReadiness)
-    mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
-    mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+    mux.HandleFunc("POST /api/validate_chirp", handlerChirpsValidate)
 
-    mux.HandleFunc("POST /", handerMethodNotAllowed)
-    mux.HandleFunc("POST /app/", handerMethodNotAllowed)
-    mux.HandleFunc("POST /api/healthz", handerMethodNotAllowed)
-    mux.HandleFunc("POST /admin/metrics", handerMethodNotAllowed)
-    mux.HandleFunc("GET /admin/reset", handerMethodNotAllowed)
-    
-    srv := &http.Server {
-        Addr: ":" + port,
+    mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+    mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+
+    srv := &http.Server{
+        Addr:    ":" + port,
         Handler: mux,
     }
 
-    log.Printf("Serving files form %s on port: %s\n", filepathRoot, port)
+    log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
     log.Fatal(srv.ListenAndServe())
-
-}
-
-func handerMethodNotAllowed (w http.ResponseWriter, r *http.Request) {
-    w.WriteHeader(http.StatusMethodNotAllowed)
-    w.Write([]byte("Method not allowed"))
 }
